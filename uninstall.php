27caf98679099f6601fc5db1d581beee4653e688
mojtababhs <mojtaba.masters@gmail.com>
2026-05-13 09:52:48 +0330

    Add files via upload

---
+++ uninstall.php
@@ -0,0 +1,11 @@
+<?php
+if (!defined('WP_UNINSTALL_PLUGIN')) exit;
+
+/*
+ * حذف کامل داده‌ها به‌صورت پیش‌فرض غیرفعال است.
+ *
+ * global $wpdb;
+ * $wpdb->query("DROP TABLE IF EXISTS {$wpdb->prefix}mobo_analytics_events");
+ * delete_option('mobo_analytics_db_version');
+ * delete_option('mobo_analytics_settings');
+ */
